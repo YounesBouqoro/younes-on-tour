2bc303497df29c7ee8fa67b5b832278f6c1d5275
YounesBouqoro <younesbouqoro@gmail.com>
2026-07-23 11:57:13 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Diese Werte kopierst du aus Firebase:
 // Projekteinstellungen → Deine Apps → Web-App → Firebase SDK-Konfiguration
 export const firebaseConfig = {
-  apiKey: "DEIN_API_KEY",
-  authDomain: "DEIN_PROJEKT.firebaseapp.com",
-  projectId: "DEIN_PROJEKT",
-  storageBucket: "DEIN_PROJEKT.appspot.com",
-  messagingSenderId: "DEINE_SENDER_ID",
-  appId: "DEINE_APP_ID"
+  apiKey: "AIzaSyCYyOy0-M45UmrN4D3mo_KGwjK7aH4F2hU",
+  authDomain: "younes-on-tour.firebaseapp.com",
+  projectId: "younes-on-tour",
+  storageBucket: "younes-on-tour.firebasestorage.app",
+  messagingSenderId: "579483034829",
+  appId: "1:579483034829:web:3d02adb17d69297056b584"
 };
 
 // Nur diese E-Mail darf Touren erstellen, ändern oder löschen.
-export const ADMIN_EMAIL = "DEINE_EMAIL@BEISPIEL.DE";
+export const ADMIN_EMAIL = "younesbouqoro@gmail.com";
